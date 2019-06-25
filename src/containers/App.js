9407bcb3a3047cf146831911baacaa5b769e9222
degalo93 <denisgalo10@gmail.com>
2019-06-25 00:57:52 -0700

    adding a basics with semantics ui

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./Navbar";
+//import Navbar from "./Navbar";
 
 import Main from "./Main"; 
 
@@ -11,7 +11,7 @@ const App = () => (
 
     <Router>
          <div className="onboarding">
-        <Navbar />
+       
          <Main /> 
          </div>
      </Router>
